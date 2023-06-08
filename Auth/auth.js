d6811b5e08e4e521adfff7a6129c8c67e2274260
Alissa Morgan <alissaam@colostate.edu>
2023-06-08 14:33:07 -0700

    added hashing to password before storage

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,31 +1,34 @@
 //Import User model
 const { error } = require("console");
 const User = require("../models/User");
-//Register
+const bcrypt = require("bcryptjs")
+//Create
 exports.register = async (req, res, next) => {
     const {username, password} = req.body;
     if (password.length < 6) {
       return res.status(400).json({ message: "Password less than 6 characters" })
     }
-    try {
+    bcrypt.hash(password, 10).then(async (hash) => {
       await User.create({
         username,
-        password,
-      }).then((user) =>
-        res.status(200).json({
-          message: "User successfully created",
-          user,
-        })
-      )
-    } catch (err) {
-      res.status(401).json({
-        message: "User not successful created",
-        error: err.message,
+        password: hash,
       })
-    }
-  }
+        .then((user) =>
+          res.status(200).json({
+            message: "User successfully created",
+             user,
+          })
+        )
+        .catch((error) =>
+          res.status(400).json({
+            message: "User not successful created",
+            error: error.message,
+          })
+        );
+    });
+  };
 
-//Login 
+//Read
 exports.login = async (req, res, next) => {
   const { username, password } = req.body
   // Check if username and password is provided
@@ -35,16 +38,21 @@ exports.login = async (req, res, next) => {
     })
   }
   try {
-    const user = await User.findOne({ username, password })
+    const user = await User.findOne({ username })
     if (!user) {
-      res.status(401).json({
+      res.status(400).json({
         message: "Login not successful",
         error: "User not found",
       })
     } else {
-      res.status(200).json({
-        message: "Login successful",
-        user,
+      // comparing given password with hashed password
+      bcrypt.compare(password, user.password).then(function (result) {
+        result
+          ? res.status(200).json({
+              message: "Login successful",
+              user,
+            })
+          : res.status(400).json({ message: "Login not succesful" })
       })
     }
   } catch (error) {
@@ -55,6 +63,7 @@ exports.login = async (req, res, next) => {
   }
 }
 
+//Delete
 exports.deleteUser = async (req, res, next) => {
   const { id } = req.body
   await User.findById(id)
