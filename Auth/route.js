2f6915fdccd181f229556dc5696465fd438340cf
Alissa Morgan <alissaam@colostate.edu>
2023-06-12 21:43:46 -0600

    Revert "front end update and delete all users route"
    This reverts commit 274665a9860aba30b330beb4599df7483cce3fef.

--- Auth/route.js
+++ Auth/route.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, deleteUser, deleteAllUsers, getUsers } = require("./Auth");
+const { register, login, deleteUser } = require("./Auth");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/deleteUser").delete(deleteUser);
-router.route("/deleteAllUsers").delete(deleteAllUsers);
-router.route("/getUsers").get(getUsers);
+router.route("/deleteUser").post(deleteUser);
 
 module.exports = router;
