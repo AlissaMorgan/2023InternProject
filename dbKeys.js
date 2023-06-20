a61e5c65fb8c7da41ce56860155879e7f2c3f4e3
Alissa Morgan <alissaam@colostate.edu>
2023-06-20 08:31:50 -0600

    fully functional encryption db with set up (but not used) keys db

--- dbKeys.js
+++ dbKeys.js
@@ -1,18 +1,18 @@
 // connect application to MongoDB
 const Mongoose = require("mongoose")
 // name of db is /internProj
-const localDB = 'mongodb://127.0.0.1:27017/encrptDB';
+const localDB = 'mongodb://127.0.0.1:27017/internProj';
 
-const connectDBEncrypt = async () => {
+const connectDBKeys = async () => {
   try{
     await Mongoose.connect(localDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  console.log("MongoDB Connected: Encrypted")
+  console.log("MongoDB Connected: Keys")
   }catch(err){
     console.log(err);
   }
 }
 
-module.exports = connectDBEncrypt
+module.exports = connectDBKeys
